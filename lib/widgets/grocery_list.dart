a4203280f88c9d5af5c51a68164587f18142c9b7
VIA2NEY <kouameangeV11@gmail.com>
2024-07-12 13:58:33 +0000

    adding get request

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,28 +15,69 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
 
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  // repose form 
+  // {
+  //  "-O1adTZPP-Nog8vZzxXi":{"category":"Dairy","name":"Test 1","quantity":5},
+  //  "-O1aifXjQcuGaycID560":{"category":"Fruit","name":"banana","quantity":2}
+  // Ce qui donne
+  //  (<String>) key : Map<String, dynamic> value , 
+  // }
+
+  void _loadItems() async{
+
+    final url = Uri.https(
+      'shopping-list-course-82756-default-rtdb.firebaseio.com',
+      'shopping-list.json'
+    );
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+
+      // Vue que dans mon GroceryItem category est la class Category et que firebase ne me ramene que le titre 
+      // donc avec le titre j'effectue une recherche qui match avec le titre pour retourner la valeur
+      final categori = categories.entries.firstWhere(
+        (catItem) => catItem.value.name == item.value['category']
+      ).value;
+
+      loadedItems.add(
+        GroceryItem(
+          id: item.key, 
+          name: item.value['name'], 
+          quantity: item.value['quantity'], 
+          category: categori
+        )
+      );
+    }
+
+    setState(() {
+      _groceryItems = loadedItems;
+    });
+    
+  }
 
   void _addItem () async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
+     await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(builder: (ctx){
         return const NewItem();
       })
     );
 
-    if (newItem == null) {
-      return;
-    }
-    
-    setState (() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item){
     setState (() {
       _groceryItems.remove(item);
     });
+  }
+
+  @override
+  void initState() {
+    _loadItems();
+    super.initState();
   }
 
   @override
